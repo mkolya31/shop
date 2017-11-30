6b879d01d20d108a9becfb58155dc470c1df14f6
mkolya31 <colyachenko@yandex.ru>
2017-11-30 13:26:26 +0300

    Изменены модели товаров и картинок товаров

--- products/models.py
+++ products/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True, default=None)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     product_category = models.ForeignKey(ProductCategory, blank=True, null=True, default=None)
+    short_description = models.TextField(blank=True, null=True, default=None)
     description = models.TextField(blank=True, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
@@ -33,6 +34,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
     image = models.ImageField(upload_to='products_images/')
+    is_main = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
