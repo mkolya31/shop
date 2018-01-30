c15ace39ca9d0c6c4d55771960ef2d854009adff
mkolya31 <colyachenko@yandex.ru>
2018-01-30 09:15:41 +0300

    30.01.18

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from .models import ProductInBasket
+from django.shortcuts import render
 
 
+#  обновление корзины на всех страницах магазина
 def basket_adding(request):
     return_dict = dict()
     session_key = request.session.session_key
@@ -9,12 +11,20 @@ def basket_adding(request):
     data = request.POST
     product_id = data.get("product_id")
     numb = data.get("nmb")
-    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id,
-                                                                 defaults={"number": numb})
-    if not created:
-        new_product.number += int(numb)
-        new_product.save(force_update=True)
+    is_delete = data.get("is_delete")
 
+    if is_delete == 'true':
+        product = ProductInBasket.objects.get(id=product_id)
+        product.is_active=False
+        product.save(force_update=True)
+    else:
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, is_active=True,
+                                                                 defaults={"number": numb})
+        if not created:
+            new_product.number += int(numb)
+            new_product.save(force_update=True)
+
+    #common code for 2 cases
     products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
     products_total_numb = products_in_basket.count()
     return_dict["product_total_numb"] = products_total_numb
@@ -23,9 +33,17 @@ def basket_adding(request):
 
     for item in products_in_basket:
         product_dict = dict()
+        product_dict["id"] = item.id
         product_dict["name"] = item.product.name
         product_dict["price_per_item"] = item.price_per_item
         product_dict["numb"] = item.number
         return_dict["products"].append(product_dict)
 
     return JsonResponse(return_dict)
+
+
+def checkout(request):
+    session_key = request.session.session_key
+    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
+    products_total_numb = products_in_basket.count()
+    return render(request, 'orders/checkout.html', locals())
--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function () { //работа с корзиной
     var form = $('#form_buying_product');
     console.log(form);
 
@@ -8,6 +8,10 @@ $(document).ready(function () {
         data.nmb = numb;
         var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
         data["csrfmiddlewaretoken"] = csrf_token;
+
+        if(is_delete){
+            data["is_delete"] = true
+        }
 
         var url = form.attr("action");
 
@@ -20,15 +24,23 @@ $(document).ready(function () {
             success: function (data) {
                 console.log("OK");
                 console.log(data.product_total_numb);
-                if (data.product_total_numb) {
+                if (data.product_total_numb || data.product_total_numb == 0) {
                     $('#product_total_numb').text("(" + data.product_total_numb + ")");
                     console.log(data.products);
                     $('.basket-items ul').html("");
                     $.each(data.products, function (k, v) {
                         $('.basket-items ul').append('<li><h6>' + v.name + ', ' + v.numb + 'шт. ' + 'по ' + v.price_per_item + ' руб  ' +
-                            '<a class="delete-item" href="">x</a>' +
+                            '<a class="delete-item" href="" data-product_id="'+v.id+'">x</a>' + //  href always must to be href="" data-product_id="'+v.id+'"
                             '</h6></li>');
-                    })
+                    });
+                    $('.basket-items ul').append('                                <form>\n' +
+                        '                                    <input class="btn btn-success" type="button" value="Оформить заказ"\n' +
+                        '                                           onclick="javascript:window.location=\'/checkout\'"/>\n' +
+                        '                                </form>');
+                    if (data.product_total_numb == 0) {
+                        $('.basket-items ul').html("");
+                        $('.basket-items ul').append('<h4 >      Корзина пуста!</h4>');
+                    }
                 }
             },
             error: function () {
@@ -55,6 +67,34 @@ $(document).ready(function () {
     });
     $(document).on('click', '.delete-item', function (e) {
         e.preventDefault();
-        $(this).closest('li').remove();
-    })
+        product_id = $(this).data("product_id");
+        numb = 0;
+        basketupdating(product_id, numb, is_delete=true)
+    });
+
+    function calculatingBasketAmount() {
+        var total_order_amount = 0;
+        $('.total-product-in-basket-amount').each(function () {
+            total_order_amount = total_order_amount + parseFloat($(this).text());
+        });
+        console.log(total_order_amount);
+        $('#total_order_amount').text(total_order_amount.toFixed(2));
+    }
+
+    $(document).on('change', ".product-in-basket-nmb", function () {
+        var current_nmb = $(this).val();
+        console.log(current_nmb);
+
+        var current_tr = $(this).closest('tr');
+        var current_price = parseFloat(current_tr.find('.product-price').text()).toFixed(2);
+        console.log(current_price);
+        var total_amount = parseFloat(current_nmb * current_price).toFixed(2);
+        console.log(total_amount);
+        current_tr.find('.total-product-in-basket-amount').text(total_amount);
+
+        calculatingBasketAmount();
+    });
+
+
+    calculatingBasketAmount();
 });
